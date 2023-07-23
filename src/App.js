0c85b67d30cc68fed28f69302a8b1b771063d034
Tabassom <tabassom.latifi@gmail.com>
2023-07-23 15:15:59 +0330

    investment-calculator

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import FormInput from "./components/FormInput/FormInput";
+import Header from "./components/Header/Header";
+import Table from "./components/Table/Table";
+import { useState } from "react";
+
+function App() {
+  const [userInput, setUserInput] = useState(null);
+
+  const calculateHandler = (userInput) => {
+    setUserInput(userInput);
+  };
+
+  const yearlyData = [];
+
+  if (userInput) {
+
+    let currentSavings = +userInput['current-savings'];
+    const yearlyContribution = +userInput['yearly-contribution'];
+    const expectedReturn = +userInput['expected-return'] / 100;
+    const duration = +userInput['duration'];
+
+    for (let i = 0; i < duration; i++) {
+      const yearlyInterest = currentSavings * expectedReturn;
+      currentSavings += yearlyInterest + yearlyContribution;
+      yearlyData.push({
+        year: i + 1,
+        yearlyInterest: yearlyInterest,
+        savingsEndOfYear: currentSavings,
+        yearlyContribution: yearlyContribution,
+      });
+    }
+  }
+  
+  return (
+    <div>
+      <Header />
+      <FormInput onCalculate={calculateHandler}/>
+      {!userInput && <p style={{textAlign: 'center'}}>No investment calculated yet!</p>}
+      {userInput && <Table data={yearlyData} initialInvestment={userInput['current-savings']}/>}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Table/Table.js
@@ -0,0 +1,49 @@
+import classes from "../Table/Table.module.css";
+
+const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
+
+const Table = (props) => {
+  return (
+    <table className={classes.result}>
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Total Savings</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.data.map((data) => {
+          return (
+            <tr key={data.year}>
+              <td>{data.year}</td>
+              <td>{formatter.format(data.savingsEndOfYear)}</td>
+              <td>{formatter.format(data.yearlyInterest)}</td>
+              <td>
+                {formatter.format(
+                  data.savingsEndOfYear -
+                    props.initialInvestment -
+                    data.yearlyContribution * data.year
+                )}
+              </td>
+              <td>
+                {formatter.format(
+                  props.initialInvestment + data.yearlyContribution * data.year
+                )}
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
